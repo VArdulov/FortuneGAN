e0642d1a5f67c036e47019342e57bde3267c5a0b
Victor Ardulov <victor@ardulov.com>
2017-09-06 18:20:49 -0700

    Initial commit of data csv file, and python script which constructs.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FortuneGAN
+## The Data
+Using a mildly reformated version of the list of 'fortunes' made available through [ruanyf/fortunes](https://github.com/ruanyf/fortunes/blob/master/data/fortunes) we cleaned and constructed a csv found in the *data* folder. This makes the text easier to load and more organized for learning language models, and such.
+
+The code for the extraction of the fortune from the text file is found in the [fortune_cleaner.py](fortune_cleaner.py)
+
+#### Modified: Sept. 6, 2017
---
+++ fortune_cleaner.py
@@ -0,0 +1,33 @@
+from csv import DictWriter
+
+fortune_text = []
+if __name__ == '__main__':
+    with open('fortunes.txt', 'r') as f_file:
+        fortune_text = f_file.readlines()
+
+    csv_entries = []
+    author = 'Uknown'
+    running_text = ''
+    for line in fortune_text:
+        if line[0] != '%':
+            if '~' not in line and '\t' not in line and '        ' not in line:
+                # actual quote text
+                running_text += (" " + line.rstrip())
+            else:
+                author = line.rstrip()
+        else:
+            csv_entries.append({'fortune_text': running_text})
+            # print('Q:', running_text)
+            # print('A:', author)
+            # author = '  ~Uknown'
+            running_text = ''
+
+    with open('data/fortune_text.csv', 'w') as csv_file:
+        writer = DictWriter(csv_file, fieldnames=['fortune_text'])
+        writer.writerows(csv_entries)
+    print('All Done!')
+
+
+
+
+
