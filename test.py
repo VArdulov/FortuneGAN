1af966dc9235441bce1e5b3f73f191ab36f24134
timoffex <timoffex@gmail.com>
2017-09-26 13:44:20 -0700

    Performing 50 critic steps per 1 generator step. Using no gradient penalty. Drawing visualization of critic. After a while, the generator's distribution seems quite close to the target distribution, but it appears that the critic is often very bad.

--- test.py
+++ test.py
@@ -33,12 +33,14 @@ def fc(bottom, size, name, reuse=False):
 
 def generator_model(noise_sym, reuse=False):
     layer = relu_fc(noise_sym, 30, 'layer1', reuse=reuse)
+    layer = relu_fc(layer, 30, 'layer2', reuse=reuse)
     output = fc(layer, 1, 'layer_out', reuse=reuse)
     return output
 
 
 def critic_model(input_sym, reuse=False):
-    layer = relu_fc(input_sym, 30, 'layer1', reuse=reuse)
+    layer = relu_fc(input_sym, 60, 'layer1', reuse=reuse)
+    layer = relu_fc(layer, 60, 'layer2', reuse=reuse)
     output = fc(layer, 1, 'layer_out', reuse=reuse)
     return output
 
@@ -46,30 +48,65 @@ def sample_real_data(size):
     while True:
         yield np.random.randn(size, 1)
 
-def test_generator(generator_tensor):
-    real_hist, _ = np.histogram(np.random.randn(10000), bins=20, range=(-4, 4), density=True)
-    fake_hist, _ = np.histogram(generator_tensor.eval(feed_dict={
-        wgan.generator_noise_sym : np.random.rand(10000, 1)
-    }), bins=20, range=(-4, 4), density=True)
-
-    for hist, mssg in [(real_hist, 'real'), (fake_hist, 'fake')]:
-        print(mssg)
-        for p in [1, .8, .6, .4, .2]:
-            print('{:.3f}'.format(p), end='')
-            for h in hist:
-                if h * (20.0/8.0) >= p:
-                    print(u'   \u2588', end='')
-                else:
-                    print('    ', end='')
-            print()
-
-        print('bins:', end='')
-        for idx in range(20):
-            print('{:4}'.format(idx), end='')
+def draw_density_histogram(histogram, bin_width, max_height=1.0, num_rows=5):
+    for p in np.linspace(max_height, 0, num_rows, endpoint=False):
+        print('{:.3f}'.format(p), end='')
+        for h in histogram:
+            if h * bin_width >= p:
+                print(u'   \u2588', end='')
+            else:
+                print('    ', end='')
         print()
 
+    print('bins:', end='')
+    for idx in range(20):
+        print('{:4}'.format(idx), end='')
+    print()
 
-wgan = WGAN([1], generator_model, critic_model)
+def draw_histogram(histogram, *, max_height, num_rows):
+    draw_density_histogram(histogram, 1, max_height=max_height, num_rows=num_rows)
+
+def test_wgan(wgan):
+    real_hist, _ = np.histogram(np.random.randn(10000), bins=20, range=(-4, 4), density=True)
+
+
+    generator_output = wgan.generator_output_sym.eval(feed_dict={
+        wgan.generator_noise_sym : np.random.rand(10000, 1)
+    })
+
+    fake_hist, _ = np.histogram(generator_output, bins=20, range=(-4, 4), density=True)
+
+    critic_inputs = np.expand_dims(np.linspace(-4, 4, 200), axis=1)
+    critic_outputs = wgan.critic_output_real_sym.eval(feed_dict={
+        wgan.critic_input_real_sym : critic_inputs
+    })
+    critic_hist = np.array([critic_outputs[range(10*a, 10*a+10)].mean() for a in range(20)])
+    critic_hist -= critic_hist.min()
+    critic_hist /= critic_hist.max()
+
+
+    # Test for exploding weights.
+    print("Number of nonfinite generator outputs: {}/10000".format(np.sum(np.logical_not(np.isfinite(generator_output)))))
+    print("Number of nonfinite critic outputs: {}/200".format(np.sum(np.logical_not(np.isfinite(critic_outputs)))))
+
+
+    print("Testing the WGAN's generator and critic...")
+    print("Integral of real_hist is {}".format(
+        (real_hist * (8.0 / 20.0)).sum()
+    ))
+    print("Integral of fake_hist is {}".format(
+        (fake_hist * (8.0 / 20.0)).sum()
+    ))
+
+    print("Real distribution:")
+    draw_density_histogram(real_hist, 8.0/20.0, max_height=0.3)
+    print("Generated distribution:")
+    draw_density_histogram(fake_hist, 8.0/20.0, max_height=0.3)
+    print("Critic's histogram (low==fake, high==real):")
+    draw_histogram(critic_hist, max_height=1.0, num_rows=5)
+
+
+wgan = WGAN([1], generator_model, critic_model, gradient_penalty_factor=0)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer()) # TODO: Initialize only WGAN variables.
@@ -77,7 +114,7 @@ with tf.Session() as sess:
     with sess.as_default():
 
         for i in range(10000):
-            wgan.train(10, sample_real_data(20))
+            wgan.train(50, sample_real_data(20))
 
             if i % 1000 == 0:
-                test_generator(wgan.generator_output_sym)
+                test_wgan(wgan)
